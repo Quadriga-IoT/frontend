37891832e8eb9838f31e613bf504611beba7db22
Nilay Özkan <nilayozkan02@gmail.com>
2022-05-23 22:45:08 +0300

    Update index.js
    veri cekme

--- src/screens/Grafik/index.js
+++ src/screens/Grafik/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { View, ScrollView, Text,Image, TouchableOpacity} from 'react-native';
 import { Chart, Line, Area, HorizontalAxis, VerticalAxis } from 'react-native-responsive-linechart'
 function Grafik()
- {
+{
   const [pil, setpil] = useState([]);
   const [zaman, setzaman] = useState([]);
   useEffect(() => 
@@ -13,9 +13,11 @@ function Grafik()
     .catch(error => console.log({error}));
   }, []);
      
-    var yenipil = pil.map(function(pil){
-      return pil.id;
-    })
+    // var yenipil = pil.map(function(pil){
+    //     return pil.id;      
+    // })
+
+    var yenipil = [10,20,22,30,40,50,60,65,75,90]
   
   useEffect(() => 
   {
@@ -38,15 +40,64 @@ function Grafik()
   {   
     sondizi[i] = {x: xdata[i] , y: ydata[i]}
   }
+
+
+
+
+
+        
+  // var dizi = bilgiler.map(bilgiler => { return <View key = {bilgiler.id} > 
+  //         <Text> {bilgiler.id} </Text>
+  //      </View>} )
+    // var yenipil = pil.map(function(pil){
+    //   return pil.battery0;
+    // })
+
+     
+
+      // var yenipil = dizi.map(dizi => { return <View key = {dizi.battery0} > 
+      //   <Text> {dizi.battery0} </Text>
+
+      //  </View>} )
+  
+    // var yenipil = pil.map(pil => { return <View key = {pil.data} > 
+    //       <Text> {pil.data.battery0} </Text>
+
+    //      </View>} )
+
+    // console.log(pil);
+  // useEffect(() => 
+  // {
+  //   axios.get("http://164.92.130.208/")
+  //   .then(response => setzaman(response.data))
+  //   .catch(error => console.log({error}));
+  // }, []); 
+
+  // var count = 0;
+  // var yenizaman = zaman.map(function(zaman){
+  //   count++;
+  //   return zaman.workingTime;
+  // })
+
+  // const xdata = yenizaman;
+  // const ydata = yenipil;
+  // var i = 0;
+  // var sondizi =[];
+  // for (i = 0; i<count ; i++)
+  // {   
+  //   sondizi[i] = {x: xdata[i] , y: ydata[i]}
+  // }
  
   return(
     <ScrollView>
+      {/* {yenipil} */}
+      {/* {dizi} */}
     <Chart
     style={{ height: 300, width: 400 }}
     data = {sondizi}
     padding={{ left: 40, bottom: 20, right: 20, top: 20 }}
     xDomain={{ min: 0, max: 10 }}
-    yDomain={{ min: 0, max: 20 }}
+    yDomain={{ min: 0, max: 100 }}
     // viewport={{ size: { width: 2 } }}
     >
     
