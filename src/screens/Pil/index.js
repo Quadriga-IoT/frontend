5824991238aa2ba959e5a7134910bd344901c427
Nilay Özkan <nilayozkan02@gmail.com>
2022-06-30 15:37:38 +0300

    tablo eklenmesi

--- src/screens/Pil/index.js
+++ src/screens/Pil/index.js
@@ -2,32 +2,41 @@ import React ,{useState , useEffect} from 'react';
 import { ScrollView, View, Text, Button } from 'react-native';
 import axios from 'axios';
 import { Chart, Line, Area, HorizontalAxis, VerticalAxis } from 'react-native-responsive-linechart'
+import { version } from 'react/cjs/react.production.min';
 
 
 
 function Pil()
 {
-  const [pil, setpil] = useState([]);
-  const [zaman, setzaman] = useState([]);
+  const [veri, setveri] = useState([]);
+  // const [zaman, setzaman] = useState([]);
   useEffect(() => 
   {
     axios.get("http://164.92.130.208")
-    .then(response => setpil(response.data))
+    .then(response => setveri(response.data))
     .catch(error => console.log({error}));
   }, []);
 
   var yenipil = new Array();
    var sayi1 = 0;
-  for (i = 0; i<9 ; i++)
+  //  var veri_uzunlugu = veri.data.length;                     //////////
+  var veri_uzunlugu = 5;                 
+  for (i = 0; i< veri_uzunlugu ; i++)
    {   
-    // yenipil[i] =sayi1;
-    // sayi1++;
-    yenipil[i] = pil.data[i].battery0;
+    yenipil[i] =sayi1;
+    sayi1++;                               
+    // yenipil[i] = veri.data[i].battery0;                 //////////
    }
 
-  //   // var yenipil = pil.map(function(pil){
-  //   //     return pil.id;      
-  //   // })
+   var yenipil2 = new Array();
+  for (i = 0; i< veri_uzunlugu ; i++)
+   {   
+    yenipil2[i] =sayi1;
+    sayi1++;                                    
+    // yenipil2[i] = veri.data[i].battery;                  //////////
+   }
+
+
 
   //   for (i = 0; i<9 ; i++)
   //   {   
@@ -35,47 +44,105 @@ function Pil()
   //   }
   //   // var yenipil = [10,20,30,50,70,80]
   
-  // useEffect(() => 
-  // {
-  //   axios.get("http://164.92.130.208")
-  //   .then(response => setzaman(response.data))
-  //   .catch(error => console.log({error}));
-  // }, []); 
 
-  // // var count = 0;
+ 
+   var count = 9;  
+  //  var count = veri_uzunlugu;                //////////
+
+  //  var yenizaman = new Array();             //////////
+  var sayi = 240;
+  var b = 0;
+  //  for (i = 0; i< veri_uzunlugu ; i++)
+  //     {   
+  //       // b = veri.data[i].workingTime;
+  //       // yenizaman[i] = b/60;               ////////
+  //       sayi= sayi+10;
+  //       yenizaman[i] = sayi/60;
+  //     }
+  var yenizaman = [0,2,4,6,8,10];        
+
+  const xdata = yenizaman;
+  const ydata = yenipil;
+  var a = 0;
+  var sondizi1 =[];
+  for (a = 0; a<count ; a++)
+  {   
+    sondizi1[a] = {x: xdata[a] , y: ydata[a]}
+  }
+
+  const ydata2 = yenipil2;
+  var sondizi2 =[];
+  for (a = 0; a<count ; a++)
+  {   
+    sondizi2[a] = {x: xdata[a] , y: ydata2[a]}
+  }
+ 
+ 
+  return(
+    <View style={{ flex: 1, alignItems: 'center'}}>
+      {/* {yenipil} */}
+      {/* {dizi} */}
+    <Chart
+    style={{ height: 300, width: 380, marginTop: 50}}
+    data = {sondizi1}
+    padding={{ left: 40, bottom: 20, right: 20, top: 20 }}
+    xDomain={{ min: 0, max: 22 }}
+    yDomain={{ min: 0, max: 100 }}
+    // viewport={{ size: { width: 2 } }}
+    >
+    
+    <VerticalAxis tickCount={5} theme={{                                     //tick count ayarlanacak,renk
+       axis: { stroke: { color: '#aaa', width: 2 } },
+       ticks: { stroke: { color: '#aaa', width: 2 } },
+      labels: { formatter: v =>" °%" + String(v)    } }} />
+    <HorizontalAxis tickCount={5} theme={{ 
+      axis: { stroke: { color: '#aaa', width: 2 } },
+      ticks: { stroke: { color: '#aaa', width: 2 } },
+      labels: { formatter: (v) => v.toFixed(0) } }}/>
+    <Area theme={{ gradient: { from: { color: '#951B1B' }, to: { color: '#D35959', opacity: 0.4 } }}} />
+    <Line theme={{ stroke: { color: '#D35959', width: 5 }, scatter: { default: { width: 4, height: 4, rx: 2 }} }} />
+    </Chart>
+
+    <Chart
+    style={{ height: 300, width: 380, marginTop: 50}}
+    data = {sondizi2}
+    padding={{ left: 40, bottom: 20, right: 20, top: 20 }}
+    xDomain={{ min: 0, max: 22 }}
+    yDomain={{ min: 0, max: 100 }}
+    // viewport={{ size: { width: 2 } }}
+    >
+    
+    <VerticalAxis tickCount={5} theme={{ 
+       axis: { stroke: { color: '#aaa', width: 2 } },
+       ticks: { stroke: { color: '#aaa', width: 2 } },
+      labels: { formatter: v =>" °%" + String(v)    } }} />    
+    <HorizontalAxis tickCount={5} theme={{ 
+      axis: { stroke: { color: '#aaa', width: 2 } },
+      ticks: { stroke: { color: '#aaa', width: 2 } },
+      labels: { formatter: (v) => v.toFixed(0) } }}/>
+    <Area theme={{ gradient: { from: { color: '#951B1B' }, to: { color: '#D35959', opacity: 0.4 } }}} />     
+    <Line theme={{ stroke: { color: '#D35959', width: 5 }, scatter: { default: { width: 4, height: 4, rx: 2 }} }} />
+    </Chart>                   
+    
+    </View>  
+  )}
+
+
+
+export default Pil;
+
+
+
+
+ // // var count = 0;
   // // var yenizaman = zaman.map(function(zaman){
   // //   count++;
   // //   return zaman.id;
   // // })
 
-   var count = 9;
-   var yenizaman = new Array();
-  var sayi = 240;
-  var b = 0;
-   for (i = 0; i<9 ; i++)
-      {   
-        b = pil.data[i].workingTime;
-        yenizaman[i] = b/60;
-        // sayi= sayi+10;
-        // yenizaman[i] = sayi/60;
-      }
-  // var yenizaman = [0,2,4,6,8,10];
-
-  const xdata = yenizaman;
-  const ydata = yenipil;
-  var a = 0;
-  var sondizi =[];
-  for (a = 0; a<count ; a++)
-  {   
-    sondizi[a] = {x: xdata[a] , y: ydata[a]}
-  }
 
 
-
-
-
-        
-  // var dizi = bilgiler.map(bilgiler => { return <View key = {bilgiler.id} > 
+// var dizi = bilgiler.map(bilgiler => { return <View key = {bilgiler.id} > 
   //         <Text> {bilgiler.id} </Text>
   //      </View>} )
     // var yenipil = pil.map(function(pil){
@@ -116,38 +183,3 @@ function Pil()
   // {   
   //   sondizi[i] = {x: xdata[i] , y: ydata[i]}
   // }
- 
-  return(
-    <View style={{ flex: 1, alignItems: 'center'}}>
-      {/* {yenipil} */}
-      {/* {dizi} */}
-    <Chart
-    style={{ height: 300, width: 380, marginTop: 50}}
-    data = {sondizi}
-    padding={{ left: 40, bottom: 20, right: 20, top: 20 }}
-    xDomain={{ min: 0, max: 22 }}
-    yDomain={{ min: 0, max: 100 }}
-    // viewport={{ size: { width: 2 } }}
-    >
-    
-    <VerticalAxis tickCount={5} theme={{ 
-       axis: { stroke: { color: '#aaa', width: 2 } },
-       ticks: { stroke: { color: '#aaa', width: 2 } },
-      labels: { formatter: v =>" °%" + String(v)    } }} />
-    <HorizontalAxis tickCount={5} theme={{ 
-      axis: { stroke: { color: '#aaa', width: 2 } },
-      ticks: { stroke: { color: '#aaa', width: 2 } },
-      labels: { formatter: (v) => v.toFixed(0) } }}/>
-    <Area theme={{ gradient: { from: { color: '#951B1B' }, to: { color: '#D35959', opacity: 0.4 } }}} />
-    <Line theme={{ stroke: { color: '#D35959', width: 5 }, scatter: { default: { width: 4, height: 4, rx: 2 }} }} />
-    </Chart>
-  
-
-
-  
-    </View>  
-  )}
-
-
-
-export default Pil;
--- src/screens/YapilanGorev/index.js
+++ src/screens/YapilanGorev/index.js
@@ -1,10 +1,123 @@
-import * as React from 'react';
-import { View, Text,Image, TouchableOpacity} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, ScrollView, View, Text } from 'react-native';
+import { Table, Row, Rows } from 'react-native-table-component';
+import axios from 'axios';
 
-function YapilanGorev() {
-    return(
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Gorev</Text>
-      </View>  
-    )}
-export default YapilanGorev;
+export default class YapilanGorev extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableHead: ['Yapılan Görev', 'Tarih'],
+      tableData: [],
+      veri:{}
+    }
+  }
+
+
+
+  componentDidMount(){
+          axios.get('http://164.92.130.208')
+          .then((res)=>
+          {
+            this.setState({veri : res.data});
+            this.func();
+          })
+          .catch((e) => console.log(e))
+      }
+
+      func= () => {
+       
+        var ornek = new Array();
+        const state1 = this.state;
+        var veri_uzunlugu = state1.veri.data.length;
+        for (i = 0; i< veri_uzunlugu; i++)
+        {      
+         ornek.push([state1.veri.data[i].activeTask, state1.veri.data[i].date ]);   
+        }
+          this.setState({
+            tableData: ornek
+          })
+        }
+
+     
+
+  render() {
+    const state = this.state;
+    return (
+      <ScrollView style={styles.container}>
+        <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+          <Row data={state.tableHead} style={styles.head} textStyle={styles.text}/>
+          <Rows data={state.tableData} textStyle={styles.text}/>
+        </Table>
+
+
+      </ScrollView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+  head: { height: 40, backgroundColor: '#f1f8ff' },
+  text: { margin: 6 }
+});
+
+
+
+
+
+
+
+
+
+
+
+// import React ,{useState , useEffect} from 'react';
+// import { View, Text,Image, TouchableOpacity} from 'react-native';
+// import {TableView} from "react-native-responsive-table"
+// import axios from 'axios';
+
+// function YapilanGorev() {
+
+//   const [veri, setveri] = useState([]);
+//   useEffect(() => 
+//   {
+//     axios.get("http://164.92.130.208")
+//     .then(response => setveri(response.data))
+//     .catch(error => console.log({error}));
+//   }, []);
+
+
+//   var dizi = new Array();
+//   var veri_uzunlugu = veri.data.length;
+
+//   for (i = 0; i< veri_uzunlugu; i++)
+//    {   
+//     // dizi[i].gorev = "c";
+//     // dizi[i].tarih = "d";
+//     // dizi.push({gorev:"c" , tarih: "d" });
+//     dizi.push({gorev: veri.data[i].activeTask , tarih: veri.data[i].date });
+//     // dizi[i].gorev = veri.data[i].activeTask;
+//     // dizi[i].tarih = veri.data[i].date;
+//    }
+
+//     return(
+//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      
+ 
+//      <TableView 
+//        headers={[
+//          {
+//              name:"Yapılan Gorev",
+//              reference_key:"gorev",
+//          },
+//          {
+//              name:"Tarih",
+//              reference_key:"tarih",
+//          },]}
+//         rows={dizi}
+//        />
+
+//       </View>  
+//     )}
+// export default YapilanGorev;
